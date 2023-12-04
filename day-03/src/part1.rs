7c8fbbefc2c7a22012cf3fdfb19001e6dc651eb9
Tim <tim.g@aeronaut.org>
2023-12-05 10:59:13 +1100

    this should work??

--- day-03/src/part1.rs
+++ day-03/src/part1.rs
@@ -1,13 +1,64 @@
-use std::vec;
-
 pub fn process(input: &str) -> String {
     let lines = input.lines();
-    let symbols: Vec<Vec<usize>> = lines.clone().map(|r| extract_symbols(r)).collect();
-    let numbers: Vec<Vec<u32>> = lines.map(|r| extract_numbers(r)).collect();
-    "".to_string()
+    let mut valid_nums = vec![];
+    let symbols = lines.clone().map(|r| extract_symbols(r));
+    let numbers = lines.clone().map(|r| extract_numbers(r));
+    numbers.enumerate().for_each(|(rowi, row)| {
+        row.iter().for_each(|num| {
+            let start_index = lines
+                .clone()
+                .nth(rowi.clone())
+                .expect("is index")
+                .find(num.to_string().as_str())
+                .expect("exists");
+            let mut indexes = "".to_string();
+            if start_index != 0 {
+                indexes = format!("{}", start_index - 1).to_string()
+            }
+            for i in 0..=num.to_string().len() {
+                if start_index + i + 1 < lines.clone().nth(rowi).expect("index").len() {
+                    indexes = format!("{}{}", indexes, start_index + i)
+                }
+            }
+            let mut should_add = false;
+            if rowi == 0 {
+                symbols.clone().enumerate().for_each(|(i, syms)| {
+                    if i == rowi || i == rowi + 1 {
+                        syms.iter().for_each(|s| {
+                            if indexes.contains(s.to_string().as_str()) {
+                                should_add = true;
+                            };
+                        });
+                    }
+                });
+            } else {
+                symbols.clone().enumerate().for_each(|(i, syms)| {
+                    if i == rowi || i == rowi + 1 || i == rowi - 1 {
+                        let _ = syms.iter().for_each(|s| {
+                            if indexes.contains(s.to_string().as_str()) {
+                                should_add = true;
+                            }
+                        });
+                    }
+                });
+            }
+
+            if should_add {
+                valid_nums.push(num.clone());
+            }
+        })
+    });
+
+    let mut sum = 0;
+    dbg!(valid_nums.clone());
+    valid_nums.iter().for_each(|val| sum += val);
+
+    sum.to_string()
+    // "".to_string()
 }
 fn extract_symbols(input: &str) -> Vec<usize> {
     let symbols: Vec<usize> = input
+        .trim()
         .chars()
         .enumerate()
         .filter(|(_, c)| c != &'.' && !c.is_digit(10) && c != &' ')
@@ -16,15 +67,15 @@ fn extract_symbols(input: &str) -> Vec<usize> {
     symbols
 }
 
-fn extract_numbers(input: &str) -> Vec<u32> {
+fn extract_numbers(input: &str) -> Vec<usize> {
     let numbers = input
         .chars()
         .map(|c| if c.is_numeric() { c } else { ' ' })
         .collect::<String>()
         .split(" ")
-        .filter(|val| val.parse::<u32>().unwrap_or(0) != 0)
-        .map(|val| val.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
+        .filter(|val| val.parse::<usize>().unwrap_or(0) != 0)
+        .map(|val| val.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
     numbers
 }
 
@@ -59,22 +110,13 @@ mod tests {
     #[case("......755.", vec![755])]
     #[case("...$.*....", vec![])]
     #[case(".664.598..", vec![664,598])]
-    fn test_extract_numbers(#[case] input: &str, #[case] expected: Vec<u32>) {
+    fn test_extract_numbers(#[case] input: &str, #[case] expected: Vec<usize>) {
         assert_eq!(expected, extract_numbers(input));
     }
 
     #[rstest]
     #[case(
-        "467..114..
-    ...*......
-    ..35..633.
-    ......#...
-    617*......
-    .....+.58.
-    ..592.....
-    ......755.
-    ...$.*....
-    .664.598..",
+        "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..",
         "4361"
     )]
     fn test_process(#[case] input: &str, #[case] expected: &str) {
