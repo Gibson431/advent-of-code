882f4ac86c906c933e3d1f2c5b5af2f0d37c4027
Tim Gibson <timmaxgibson@gmail.com>
2023-12-07 15:27:48 +1100

    day6 part 2 done

--- day-06/src/part2.rs
+++ day-06/src/part2.rs
@@ -2,7 +2,7 @@ use std::iter::zip;
 
 use nom::{
     bytes::complete::tag,
-    character::complete::{self, space1},
+    character::complete::{digit1, space1},
     multi::separated_list1,
     IResult, Parser,
 };
@@ -11,44 +11,47 @@ use nom_supreme::ParserExt;
 
 #[derive(Debug, PartialEq, PartialOrd, Clone)]
 struct Race {
-    time: u32,
-    record: u32,
+    time: u64,
+    record: u64,
 }
 
 impl Race {
-    fn new(time: u32, record: u32) -> Race {
+    fn new(time: u64, record: u64) -> Race {
         Race { time, record }
     }
 }
 
-fn parse_input(input: &str) -> IResult<&str, Vec<Race>> {
+fn parse_input(input: &str) -> IResult<&str, Race> {
     let mut lines = input.lines();
     let (_, times) = tag("Time:")
         .precedes(space1)
-        .precedes(separated_list1(space1, complete::u32))
+        .precedes(separated_list1(space1, digit1))
         .parse(lines.next().unwrap())?;
 
-    let (input, distances) = tag("Distance:")
+    let (_input, distances) = tag("Distance:")
         .precedes(space1)
-        .precedes(separated_list1(space1, complete::u32))
+        .precedes(separated_list1(space1, digit1))
         .parse(lines.next().unwrap())?;
 
     let races = zip(times, distances);
-    let races = races.map(|tup| Race::new(tup.0, tup.1)).collect();
+    let races = races.fold((String::new(), String::new()), |mut acc, item| {
+        acc.0.push_str(item.0);
+        acc.1.push_str(item.1);
+        acc
+    });
 
-    Ok((input, races))
+    let race = Race::new(races.0.parse().unwrap(), races.1.parse().unwrap());
+    Ok((input, race))
 }
 
 pub fn process(input: &str) -> String {
-    let (_, races) = parse_input(input).unwrap();
-    dbg!(&races);
+    let (_, race) = parse_input(input).unwrap();
+    dbg!(&race);
 
-    let counts = races.iter().map(|r| {
-        (0..r.time)
-            .filter_map(|t| (t * (r.time - t) > r.record).then_some(t))
-            .count()
-    });
-    counts.product::<usize>().to_string()
+    let count = (0..race.time)
+        .filter_map(|t| (t * (race.time - t) > race.record).then_some(t))
+        .count();
+    count.to_string()
 }
 
 #[cfg(test)]
