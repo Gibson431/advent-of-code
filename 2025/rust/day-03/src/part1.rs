ec8685f43f3508ddb53b62d70cba62646553e996
Gibson431 <timmaxgibson@gmail.com>
2025-12-10 17:52:46 +1100

    functional day 3 part 1, half part2

--- 2025/rust/day-03/src/part1.rs
+++ 2025/rust/day-03/src/part1.rs
@@ -1,6 +1,37 @@
+use itertools::Itertools;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    Ok("day-03 - part 1".to_string())
+pub fn process(input: &str) -> miette::Result<String> {
+    let banks = input.lines();
+
+    let jolts =
+        banks.fold(0, |acc, bank| acc + process_bank(bank));
+
+    Ok(jolts.to_string())
+}
+
+fn process_bank(bank: &str) -> u32 {
+    let bank_as_num: Vec<_> = bank
+        .chars()
+        .map(|v| v.to_digit(10).unwrap())
+        .collect();
+
+    let first_chunk =
+        bank_as_num[0..(bank.len() - 1)].iter();
+    let first = first_chunk.clone().max().unwrap();
+    let (idx, first) = first_chunk
+        .enumerate()
+        .find(|(_, x)| *x == first)
+        .unwrap();
+
+    let second = bank_as_num[idx + 1..bank.len()]
+        .iter()
+        .sorted()
+        .rev()
+        .next()
+        .unwrap();
+
+    first * 10 + second
 }
 
 #[cfg(test)]
@@ -9,9 +40,21 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
+        assert_eq!("357", process(input)?);
         Ok(())
+    }
+
+    #[rstest::rstest]
+    #[case("12333345", 45)]
+    #[case("989879612", 99)]
+    fn process_bank_test(
+        #[case] input: &str,
+        #[case] expected: u32,
+    ) {
+        assert_eq!(expected, process_bank(input));
     }
 }
--- 2025/rust/day-03/src/part2.rs
+++ 2025/rust/day-03/src/part2.rs
@@ -1,6 +1,42 @@
+use itertools::Itertools;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    Ok("day-03 - part 2".to_string())
+pub fn process(input: &str) -> miette::Result<String> {
+    let banks = input.lines();
+
+    let jolts =
+        banks.fold(0, |acc, bank| acc + process_bank(bank));
+
+    Ok(jolts.to_string())
+}
+
+fn process_bank(bank: &str) -> u64 {
+    let bank_as_num: Vec<_> = bank
+        .chars()
+        .map(|v| v.to_digit(10).unwrap() as u64)
+        .collect();
+
+    let mut last_idx = 0;
+    let mut acc = 0;
+    for i in (0..12).rev() {
+        let (idx, jolt) = max_in_iter(
+            bank_as_num[last_idx..bank_as_num.len() - i]
+                .iter(),
+        );
+        last_idx = idx;
+        acc += jolt * i as u64;
+    }
+
+    acc
+}
+
+fn max_in_iter<'a>(
+    mut iter: impl Iterator<Item = &'a u64> + Clone,
+) -> (usize, u64) {
+    let num = iter.clone().max().unwrap();
+    let idx = iter.position(|x| x == num).unwrap();
+
+    (idx, num.clone())
 }
 
 #[cfg(test)]
@@ -9,9 +45,21 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
+        assert_eq!("3121910778619", process(input)?);
         Ok(())
+    }
+
+    #[rstest::rstest]
+    #[case("987654321111111", 987654321111)]
+    #[case("811111111111119", 811111111119)]
+    fn process_bank_test(
+        #[case] input: &str,
+        #[case] expected: u64,
+    ) {
+        assert_eq!(expected, process_bank(input));
     }
 }
